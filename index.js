d4c2bcb2aebc50ae73b09cd4ecdbfac35644b543
Mercy <maritimmercy8@gmail.com>
2025-03-20 20:04:16 +0300

    added post method

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your code here
+
+
+function submitData(name, email) {
+    const formData = {
+        name: name,
+        email: email,
+    };
+
+    // method: "POST" is missing from the object below
+
+    const configurationObject = {
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(formData),
+        method: "POST",
+
+
+    };
+
+
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            console.log(object);
+            document.getElementById("result").innerHTML = object.id
+        })
+        .catch(function (error) {
+            alert("Bad things! Ragnarők!");
+            console.log(error.message);
+            document.getElementById("result").innerHTML = error.message
+        });
+}
